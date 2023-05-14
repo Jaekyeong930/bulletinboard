34e4817a2991413b1ea41f8994ec4040b9c11fe3
jkkim <jaekyeong930@gmail.com>
2023-05-14 21:04:07 +0900

    setted up bulletinboard project

---
+++ bulletinboard/src/test/java/jkkim/bulletinboard/BulletinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package jkkim.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
